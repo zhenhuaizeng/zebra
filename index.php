08f1fffe6cef6af998a88cc0b789ff87fb73f9a3
Ron Nguyen <ronnguyen1011@gmail.com>
2023-02-02 16:14:09 -0500

    set up index page

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+//This is my CONTROLLER for the hello project
+
+//Turn on error reporting
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+// Create an instance of the Base class
+$f3 = Base::instance();
+//Java equivalent -> Base f3 = new Base();
+
+// Define a default route ("Home page" for hello project)
+$f3->route('GET /', function(){
+    echo '<h1>Zebra!</h1>';
+//    $view = new Template();
+//    echo $view->render('views/Home.html');
+});
+
+//Run Fat-Free
+$f3->run();
+//Java -> f3.run();
